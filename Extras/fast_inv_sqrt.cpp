8dc212a7fea715bda21ff70368ab8f345cf2d8ad
lwob <bobbymonstereater@gmail.com>
2018-01-16 17:53:56 -0500

    Added some more magic

---
+++ Extras/fast_inv_sqrt.cpp
@@ -0,0 +1,23 @@
+#include <stdin.h>
+
+
+/*
+ * It's basically newton's method with some cool bithacks
+ * But we can think of it as magic
+ */
+float Q_rsqrt( float number )
+{
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( long * ) &y;  // evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 ); // what the fuck? 
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+	return y;
+}
