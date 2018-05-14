bba8504eb8524359b0bdf549b4cb87acec56fd2b
lwob <wang.kevin.email@gmail.com>
2018-05-14 15:37:27 -0400

    It works boys

--- TreeDataStructures/persistentSegmentTree.cpp
+++ TreeDataStructures/persistentSegmentTree.cpp
@@ -20,9 +20,9 @@ class PersistentSegmentTree {
         
         bool childrenExist = true;
     public:
-        SegmentTree(int left_range, int right_range, T arr[]) :
-            left{left_idx >= right_idx ? nullptr : new SegmentTree(left_idx, (right_idx + left_idx)/2, values)},
-    		right{left_idx >= right_idx ? nullptr : new SegmentTree((right_idx + left_idx)/2 + 1, right_idx, values)},
+        SegmentTree(int left_idx, int right_idx, T values[]) :
+            left_child{left_idx >= right_idx ? nullptr : new SegmentTree(left_idx, (right_idx + left_idx)/2, values)},
+    		right_child{left_idx >= right_idx ? nullptr : new SegmentTree((right_idx + left_idx)/2 + 1, right_idx, values)},
     		
     		left_idx{left_idx},
     		right_idx{right_idx} {
@@ -31,8 +31,8 @@ class PersistentSegmentTree {
     		if (left_idx < right_idx) {
     			// printf("[%d, %d] made [%d, %d] = %d and [%d %d] = %d\n", left_idx, right_idx, left_idx, mid, getVal(left_idx, mid), mid+1, right_idx, getVal(mid+1, right_idx));
     			// Update current value
-    			value = left->getVal(left_idx, (right_idx + left_idx)/2) +
-    					right->getVal((right_idx + left_idx)/2+1, right_idx);
+    			value = left_child->getVal(left_idx, (right_idx + left_idx)/2) +
+    					right_child->getVal((right_idx + left_idx)/2+1, right_idx);
     		}
     		else if (left_idx == right_idx) {
     			// Our current value
@@ -43,7 +43,7 @@ class PersistentSegmentTree {
             left_child{left_child},
             right_child{right_child},
             left_idx{left_range},
-            right_idx{right_range}, {
+            right_idx{right_range} {
             
             // Update the value of the current node
             value = left_child->getVal() + right_child->getVal();
@@ -59,23 +59,20 @@ class PersistentSegmentTree {
         }
         
         SegmentTree(SegmentTree* tree, bool childrenExist = true) : 
+            left_child{tree->left_child},
+            right_child{tree->right_child},
+            left_idx{tree->left_idx},
+            right_idx{tree->right_idx},
+            value{tree->value},
+            delta{tree->delta},
             childrenExist{childrenExist}{
-                
-            // Copy all of the values 
-            left_child = tree->left_child;
-            right_child = tree->right_child;
-            
-            left_idx = tree->left_idx;
-            right_idx = tree->right_idx;
-            
-            value = tree->value;
-            deltas = tree->value;
+            // NOTHING TO SEE HERE
         }
         
         // Update current node and push deltas down
-        void pushLazy(T val = T()) {
+        SegmentTree* pushLazy(T val = T()) {
             SegmentTree* st = new SegmentTree(this, false);
-            st.updateLazy(val);
+            st->updateLazy(val);
             
             return st;
         }
@@ -124,8 +121,8 @@ class PersistentSegmentTree {
             
             // Make a new node if we did get here
             return new SegmentTree(
-                left->updateRange(left_range, right_range, val), // Update the left and right
-                right->updateRange(left_range, right_range, val), // and then links to the other segments
+                left_child->updateRange(left_range, right_range, val), // Update the left and right
+                right_child->updateRange(left_range, right_range, val), // and then links to the other segments
                 left_idx, right_idx, // Range 
                 value + newDelta, // Current value
                 delta // Lazy delta
@@ -144,22 +141,22 @@ class PersistentSegmentTree {
     		// Push lazy deltas into children
     		if (delta){
                 if (childrenExist) {
-                    if (left) {
-                        left->updateLazy(delta);
+                    if (left_child) {
+                        left_child->updateLazy(delta);
                     }
-                    if (right) {
-                        right->updateLazy(delta);
+                    if (right_child) {
+                        right_child->updateLazy(delta);
                     }
                 }
                 // If both are kill
                 else {
-                    if (left) {
-                        left = new SegmentTree(left, false);
-                        left->updateLazy(delta);
+                    if (left_child) {
+                        left_child = new SegmentTree(left_child, false);
+                        left_child->updateLazy(delta);
                     }
-                    if (right) {
-                        right = new SegmentTree(right, false);
-                        right->updateLazy(delta);
+                    if (right_child) {
+                        right_child = new SegmentTree(right_child, false);
+                        right_child->updateLazy(delta);
                     }
                 }
     			delta = T();
@@ -170,29 +167,57 @@ class PersistentSegmentTree {
     			return value;
     		}
     		
-    		return left->getVal(left_range, right_range) + right->getVal(left_range, right_range);	
+    		return left_child->getVal(left_range, right_range) + right_child->getVal(left_range, right_range);	
+    	}
+        
+        void printTree() {
+    		printf("eval([%d, %d]) = %d\n", left_idx, right_idx, value);
+    		if (left_child)
+    			left_child->printTree();
+    		if (right_child)
+    			right_child->printTree();
     	}
     };
     
     vector<SegmentTree*> treeArr;
+    int cur_idx = 0;
 public:
     
     PersistentSegmentTree(int size, T arr[]) {
-        treeArr.push_back(make_shared(new SegmentTree(0, size-1, arr)));
+        treeArr.push_back(new SegmentTree(0, size-1, arr));
     }
     
+    T query(int left_idx, int right_idx) {
+        return treeArr[cur_idx]->getVal(left_idx, right_idx);
+    }
     
+    T query(int left_idx, int right_idx, int k) {
+        return treeArr[k-1]->getVal(left_idx, right_idx);
+    }
+    
+    void updateRange(int left_idx, int right_idx, T val) {
+        treeArr.push_back(treeArr[cur_idx]->updateRange(left_idx, right_idx, val));
+        ++cur_idx;
+    }
+    
+    void printTree() {
+        for (auto t : treeArr) {
+            t->printTree();
+            printf("\n");
+        }
+    }
 };
 
 int main() {
-	int a[] = {1, 2, 3, 4, 5, 6, 7};
+	int a[] = {1, 2, 3, 4, 5, 6, 7, 8};
 	
-	SegmentTree<int>* stree = new SegmentTree<int>(0, 6, a);
+	PersistentSegmentTree<int>* stree = new PersistentSegmentTree<int>(8, a);
 	
 	stree->printTree();
-	
+	cout << stree->query(3, 6) << endl;
+    
 	stree->updateRange(2, 5, 10);
-	cout << stree->getVal(3, 6) << endl;
+	cout << stree->query(3, 6) << endl;
 	stree->printTree();
 	
 	delete stree;
